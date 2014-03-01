fae7226899f834e33c663c4ff3cd514c29f5d664
tk0miya <i.tkomiya@gmail.com>
2014-03-01 15:11:20 +0900

    Simplify tox.ini (refer extra_require[testing])

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     include_package_data=True,
     install_requires=install_requires,
     extras_require=dict(
-        test=[
+        testing=[
             'mock',
             'nose',
         ],
